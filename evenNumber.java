47a72113d2862dc352bc14b820c7a4f969fcc1a7
Manish <manishkadwal2@gmail.com>
2024-02-28 14:19:45 +0530

    Even Number Submit

---
+++ evenNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class evenNumber{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+	
+
+if(num%2==0){
+System.out.println("odd number");
+}
+else{
+System.out.println("even number") ;
+
+}
+}
+}
