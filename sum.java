0dbc2f3b21a44a503dac8923a67733db16d34e86
Manish <manishkadwal2@gmail.com>
2024-02-28 14:42:09 +0530

    done

---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum{
+public static void main (String args[]){
+int a=120;
+int b=10;
+System.out.println(a%b);
+System.out.println(a/b);
+System.out.println(a+b);
+String c ="10";
+String d="12";
+System.out.println("c+d");
+
+
+}
+}
